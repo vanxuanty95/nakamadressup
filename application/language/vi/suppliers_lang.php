b2220f6980b5781b4087341d11a9269032f8b724
XUANTY-PC\vanxuanty <vanxuanty95@gmail.com>
2020-03-03 19:55:03 +0700

    Update source

---
+++ application/language/vi/suppliers_lang.php
@@ -0,0 +1,23 @@
+<?php 
+
+$lang["suppliers_account_number"] = "Số hiệu tài khoản";
+$lang["suppliers_agency_name"] = "Tên đại lý";
+$lang["suppliers_cannot_be_deleted"] = "Không thể xóa các Người ký gửi đã chọn. Một hay nhiều có Bán hàng.";
+$lang["suppliers_category"] = "";
+$lang["suppliers_company_name"] = "Tên công ty";
+$lang["suppliers_company_name_required"] = "Trường tên công ty là bắt buộc.";
+$lang["suppliers_confirm_delete"] = "Bạn chắc chắn muốn xóa các Người ký gửi được chọn không?";
+$lang["suppliers_confirm_restore"] = "Bạn chắc chắn muốn hoàn lại các Người ký gửi được chọn không?";
+$lang["suppliers_cost"] = "";
+$lang["suppliers_error_adding_updating"] = "Gặp lỗi khi cập nhật hay thêm Người ký gửi.";
+$lang["suppliers_goods"] = "";
+$lang["suppliers_new"] = "Người ký gửi mới";
+$lang["suppliers_none_selected"] = "Bạn chưa chọn Người ký gửi để xóa.";
+$lang["suppliers_one_or_multiple"] = "Người ký gửi";
+$lang["suppliers_successful_adding"] = "Bạn đã thêm Người ký gửi thành công";
+$lang["suppliers_successful_deleted"] = "Bạn đã xóa thành công";
+$lang["suppliers_successful_updating"] = "Bạn đã cập nhật Người ký gửi thành công";
+$lang["suppliers_supplier"] = "Người ký gửi";
+$lang["suppliers_supplier_id"] = "Mã số";
+$lang["suppliers_tax_id"] = "";
+$lang["suppliers_update"] = "Cập nhật Người ký gửi";
