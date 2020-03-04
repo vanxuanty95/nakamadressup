cf453ecdf854f0bf98745f6659cd43739e00bd0f
XUANTY-PC\vanxuanty <vanxuanty95@gmail.com>
2020-03-04 20:29:00 +0700

    no message

--- application/views/receivings/receiving.php
+++ application/views/receivings/receiving.php
@@ -334,7 +334,7 @@ if (isset($success)) {
 											<script type="text/javascript">
 												$(function() {
 													$('#datetimepicker1').datetimepicker({
-														format: 'L',
+														pickTime: false,
 													});
 												});
 											</script>
